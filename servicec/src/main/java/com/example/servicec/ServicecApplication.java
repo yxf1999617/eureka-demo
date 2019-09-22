dae516c1b3391dedd664ee58c338ede0f244bd24
yxf1999617 <1399982146@qq.com>
2019-09-22 16:36:10 +0800

    Initial commit

---
+++ servicec/src/main/java/com/example/servicec/ServicecApplication.java
@@ -0,0 +1,15 @@
+package com.example.servicec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServicecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicecApplication.class, args);
+	}
+
+}
